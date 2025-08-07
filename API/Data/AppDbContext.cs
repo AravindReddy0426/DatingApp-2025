002195efca5b1f55795b48cc6b2789d420394c5f
AravindReddy <aravindreddy0426@gmail.com>
2025-08-07 08:31:56 +0530

    End of section 1

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<AppUser> Users { get; set; }
+}
